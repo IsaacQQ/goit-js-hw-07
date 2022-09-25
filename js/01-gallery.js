2c50b155f9b3d2c6e0e9b1c1b1b2a0a560f23fb3
Игорь <bujhtyz228@gmail.com>
2022-09-25 19:45:55 +0300

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const gallery = document.querySelector('.gallery')
+const imgs = []
+
+galleryItems.forEach(el => {
+    const gItem = document.createElement('div')
+    gItem.className = 'gallery__item'
+    const gLink = document.createElement('a')
+    gLink.className = 'gallery__link'
+    gLink.href = el.original
+    const gImg = document.createElement('img')
+    gImg.className = 'gallery__image'
+    gImg.src = el.preview
+    gImg.setAttribute('data-source', el.original)
+    gImg.alt = el.description
+
+
+    gItem.append(gLink)
+    gLink.append(gImg)
+    imgs.push(gItem)
+})
+
+gallery.append(...imgs)
+
+gallery.addEventListener('click', onClick => {
+onClick.preventDefault()
+if(onClick.target.nodeName !== 'IMG'){
+    return
+}
+
+const imgSelect = onClick.target.getAttribute('data-source')
+
+const instance = basicLightbox.create(`
+<img src="${imgSelect}" width="800" height="600">
+`)
+
+instance.show()
+
+gallery.addEventListener('keydown', e => {
+    if (e.key === 'Escape'){
+        instance.close()
+    }
+})
+})
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery')
+const imgs = []
+
+galleryItems.forEach(el => {
+    const gLink = document.createElement('a')
+    gLink.className = 'gallery__link'
+    gLink.href = el.original
+    const gImg = document.createElement('img')
+    gImg.className = 'gallery__image'
+    gImg.src = el.preview
+    gImg.setAttribute('title', el.description)
+    gImg.alt = el.description
+
+
+   gLink.append(gImg)
+   imgs.push(gLink)
+})
+
+gallery.append(...imgs)
+
+new SimpleLightbox('.gallery a', {
+    captionDelay: 250
+})
