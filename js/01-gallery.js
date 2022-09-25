17b115995bd6c9b73082191e9a4f345920314eed
Игорь <bujhtyz228@gmail.com>
2022-09-25 21:46:16 +0300

    prettier added

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,47 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 // console.log(galleryItems);
-const gallery = document.querySelector('.gallery')
-const imgs = []
+const gallery = document.querySelector(".gallery");
+const imgs = [];
 
-galleryItems.forEach(el => {
-    const gItem = document.createElement('div')
-    gItem.className = 'gallery__item'
-    const gLink = document.createElement('a')
-    gLink.className = 'gallery__link'
-    gLink.href = el.original
-    const gImg = document.createElement('img')
-    gImg.className = 'gallery__image'
-    gImg.src = el.preview
-    gImg.setAttribute('data-source', el.original)
-    gImg.alt = el.description
+galleryItems.forEach((el) => {
+  const gItem = document.createElement("div");
+  gItem.className = "gallery__item";
+  const gLink = document.createElement("a");
+  gLink.className = "gallery__link";
+  gLink.href = el.original;
+  const gImg = document.createElement("img");
+  gImg.className = "gallery__image";
+  gImg.src = el.preview;
+  gImg.setAttribute("data-source", el.original);
+  gImg.alt = el.description;
 
+  gItem.append(gLink);
+  gLink.append(gImg);
+  imgs.push(gItem);
+});
 
-    gItem.append(gLink)
-    gLink.append(gImg)
-    imgs.push(gItem)
-})
+gallery.append(...imgs);
 
-gallery.append(...imgs)
+gallery.addEventListener("click", (onClick) => {
+  onClick.preventDefault();
+  if (onClick.target.nodeName !== "IMG") {
+    return;
+  }
 
-gallery.addEventListener('click', onClick => {
-onClick.preventDefault()
-if(onClick.target.nodeName !== 'IMG'){
-    return
-}
+  const imgSelect = onClick.target.getAttribute("data-source");
 
-const imgSelect = onClick.target.getAttribute('data-source')
-
-const instance = basicLightbox.create(`
+  const instance = basicLightbox.create(`
 <img src="${imgSelect}" width="800" height="600">
-`)
+`);
 
-instance.show()
+  instance.show();
 
-gallery.addEventListener('keydown', e => {
-    if (e.key === 'Escape'){
-        instance.close()
+  gallery.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      instance.close();
     }
-})
-})
+  });
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,27 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
-const gallery = document.querySelector('.gallery')
-const imgs = []
+const gallery = document.querySelector(".gallery");
+const imgs = [];
 
-galleryItems.forEach(el => {
-    const gLink = document.createElement('a')
-    gLink.className = 'gallery__link'
-    gLink.href = el.original
-    const gImg = document.createElement('img')
-    gImg.className = 'gallery__image'
-    gImg.src = el.preview
-    gImg.setAttribute('title', el.description)
-    gImg.alt = el.description
+galleryItems.forEach((el) => {
+  const gLink = document.createElement("a");
+  gLink.className = "gallery__link";
+  gLink.href = el.original;
+  const gImg = document.createElement("img");
+  gImg.className = "gallery__image";
+  gImg.src = el.preview;
+  gImg.setAttribute("title", el.description);
+  gImg.alt = el.description;
 
+  gLink.append(gImg);
+  imgs.push(gLink);
+});
 
-   gLink.append(gImg)
-   imgs.push(gLink)
-})
+gallery.append(...imgs);
 
-gallery.append(...imgs)
-
-new SimpleLightbox('.gallery a', {
-    captionDelay: 250
-})
+new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+});
